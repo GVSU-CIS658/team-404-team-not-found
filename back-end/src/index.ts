bf80c5ceb58c93d25e3539157a09e41bafe8d341
jangilisai-nishith <jangilis@mail.gvsu.edu>
2026-04-26 18:29:47 -0400

    Update index.ts

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,4 +2,4 @@ export { createEvent } from "./events/createEvent";
 export { updateEvent } from "./events/updateEvent";
 export { deleteEvent } from "./events/deleteEvent";
 export { registerForEvent } from "./registrations/registerForEvent";
-export { cancelRegistration } from "./registrations/cancelRegistration";
+export { cancelRegistration } from "./registrations/cancelRegistration"; 
