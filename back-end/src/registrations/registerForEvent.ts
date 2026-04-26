1dbb87802b6e08bad6ac0968048107dc4ce3d8f7
jangilisai-nishith <jangilis@mail.gvsu.edu>
2026-04-26 18:29:38 -0400

    Update registerForEvent.ts

--- back-end/src/registrations/registerForEvent.ts
+++ back-end/src/registrations/registerForEvent.ts
@@ -1,4 +1,4 @@
-import { onCall, HttpsError } from "firebase-functions/v2/https";
+import { onCall, HttpsError } from "firebase-functions/v2/https"; 
 import { admin, db } from "../shared/admin";
 import { requireAuth } from "../shared/auth";
 
